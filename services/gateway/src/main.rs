8b1a1c4629c281e06e1752a053c55a19cf34b700
Dmitriy Chukhno <dmitr1sdadaya@protonmail.com>
2023-12-30 18:33:55 +0100

    [STYLE] Lint code

--- services/gateway/src/main.rs
+++ services/gateway/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder, middleware};
 use futures::future;
 
 #[get("/")]
@@ -6,27 +6,20 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let http_server = HttpServer::new(|| {
         App::new()
-            .wrap(actix_web::middleware::Compress::default())
+            .wrap(middleware::Compress::default())
             .service(hello)
-            .service(echo)
     })
     .bind(("0.0.0.0", 80))?
     .run();
 
     let https_server = HttpServer::new(|| {
         App::new()
-            .wrap(actix_web::middleware::Compress::default())
+            .wrap(middleware::Compress::default())
             .service(hello)
-            .service(echo)
     })
     .bind(("0.0.0.0", 443))?
     .run();
