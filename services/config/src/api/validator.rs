e3c61b6e6bca625d008900305c3044c9af80b616
Dmitriy Chukhno <dmitr1sdadaya@protonmail.com>
2024-01-16 16:51:58 +0100

    [STYLE] Lint code

--- services/config/src/api/validator.rs
+++ services/config/src/api/validator.rs
@@ -1,8 +1,7 @@
 use validator::ValidationError;
 
 pub fn validate_next_page(next_page: &str) -> Result<(), ValidationError> {
-    base64_url::decode(next_page)
-        .map_err(|_err| ValidationError::new("invalid token"))?;
+    base64_url::decode(next_page).map_err(|_err| ValidationError::new("invalid token"))?;
 
     Ok(())
 }
